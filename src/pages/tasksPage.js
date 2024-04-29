94ab700e736e1e9aa00d52950d8616b6db1bc0f3
1Ai1 <azamatov.11.08@gmail.com>
2024-04-29 19:18:55 +0500

    Fundaments

---
+++ src/pages/tasksPage.js
@@ -0,0 +1,15 @@
+function TasksPage() {
+  return (
+    <div>
+      <h1>Tasks Page</h1>
+      <div className="spheres">
+        <div className="sphere">
+          <div>Programming</div>
+          <div>Course card</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TasksPage;
